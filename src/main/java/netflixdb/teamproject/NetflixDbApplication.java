5dda471444f0deb2c4b65330249d664b4ffa0af9
BaeSangUn <zyxtkddns@naver.com>
2022-10-25 18:30:52 +0900

    내 넷플릭스 디비

---
+++ src/main/java/netflixdb/teamproject/NetflixDbApplication.java
@@ -0,0 +1,13 @@
+package netflixdb.teamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/netflixdb/teamproject/NetflixDbApplicationTests.java
@@ -0,0 +1,13 @@
+package netflixdb.teamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
